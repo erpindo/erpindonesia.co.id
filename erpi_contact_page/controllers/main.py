bca458198c16fdf812c37b82d4702503b37bdc9c
Bima Wijaya <bimajatiwijaya@gmail.com>
2018-08-27 12:38:03 +0700

    [ADD] Add contact feature

--- erpi_contact_page/controllers/main.py
+++ erpi_contact_page/controllers/main.py
@@ -5,9 +5,7 @@ from odoo.http import request
 
 class ContactNameCard(http.Controller):
 
-    @http.route('/contact/', auth='public')
+    @http.route('/contacts/', auth='public',website=True)
     def contact(self, **kw):
-        values = {
-            'content': 'test',
-        }
-        return http.request.render('erpi_contact_page.catp_nc_page_template', values)
+        return http.request.render('erpi_contact_page.catp_nc_page_template')
+
