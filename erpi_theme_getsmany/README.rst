d82b2ffc659e9e12b4fd8335f03f586e190612cb
candra.ux <candra.erpi@gmail.com>
2018-11-01 11:26:52 +0700

    [add] Initial Setup Theme
    1. Initial Setup Theme
    2. Add Footer and Header

---
+++ erpi_theme_getsmany/README.rst
@@ -0,0 +1,11 @@
+Usage
+=====
+Company Profile Theme Getsmany
+
+Setting
+=======
+This theme install as full theme, not module. -catp-
+
+Contributors
+============
+1. Candra <db.candra@gmail.com>
---
+++ erpi_theme_getsmany/__manifest__.py
@@ -0,0 +1,32 @@
+# -*- encoding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Company Profile Purple Theme',
+    'summary': 'Odoo Theme for Getsmany Company Profile',
+    'category': 'Theme',
+    'sequence': 900,
+    'version': '1.0',
+    'depends': ['website'],
+    'version':'11.0.1.0',
+    'author': "PT. ERP Indonesia",
+    'website': "https://www.erpindonesia.co.id",
+    'data': [
+        'views/test.xml',
+        'views/assets.xml',
+        'views/layout.xml',
+        'views/snippets.xml',
+        'views/snip_options.xml',
+        # 'views/home.xml',
+        # 'views/about.xml',
+        # 'views/services.xml',
+        # 'views/replace.xml',
+    ],
+    'images': [
+        'static/description/cover.jpg',
+		'static/description/icon.png'
+    ],
+    'application': True,
+    'installable':True,
+    'auto_install':False,
+}
